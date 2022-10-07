1ae3cad0ad7e14837352264cc9d264b57be700c3
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2022-10-07 20:27:04 +0100

    All done

--- login.js
+++ login.js
@@ -17,7 +17,7 @@ const change = ()=>{
 console.log(student);
 const signIn = ()=>{
     if (student.email === email.value && student.passWord === password.value) {
-        window.location.href = 'index3.html'
+        window.location.href = 'dashboard.html'
     }
     else {
         alert("Email not found, pls check and try again or create an account if you haven't")
