c0842113797f7fc59e3e48075b31d9a1459048bf
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2022-10-07 19:42:20 +0100

    dashboard updated

--- index3.js
+++ index3.js
@@ -2,4 +2,12 @@ let unParsedStudent = localStorage.getItem('student')
 let student = JSON.parse(unParsedStudent)
 console.log(student);
 let nameDiv = document.querySelector('.name')
+let email = document.querySelector('.email')
+let matNo = document.querySelector('.matNo')
+let age = document.querySelector('.age')
+let regDate = document.querySelector('.regDate')
+let phone = document.querySelector('.phone')
 nameDiv.innerText = `${student.firstName} ${student.lastName}!`
+email.innerText += `${student.email}`
+age.innerText += `${student.age}`
+phone.innerText += `${student.phone}`
